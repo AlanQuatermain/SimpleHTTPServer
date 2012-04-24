a6265c7f7790c3621fd037848def82ef03f8621e
Jim Dovey <jimdovey@mac.com>
2012-04-24 14:39:26 -0400

    Initial Commit

---
+++ SimpleHTTPServer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SimpleHTTPServer
+//
+//  Created by Jim Dovey on 2012-04-24.
+//  Copyright (c) 2012 Kobo Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
